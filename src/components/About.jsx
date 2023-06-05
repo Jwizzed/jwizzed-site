114a037d5be51b9b4cc49fb4eb64fa1006e6faed
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-06-05 12:51:23 +0700

    added about me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,20 +2,47 @@ import React from 'react';
 
 function About() {
     return <section id="about">
-        <div className="about">
-            <h1>About Me</h1>
-            <p>Hello, I'm JJ, a software and knowledge engineering student at Kasetsart University. My main areas of interest revolve around AI, machine learning, and data processing.</p>
-            <p>"My life goal is to create a must-have thing for everyone, not just something nice to have."</p>
-        </div>
-        <div className="skill">
-            <h3>Python</h3>
-            <h3>Java</h3>
-            <h3>JavaScript</h3>
-            <h3>C++</h3>
-            <h3>HTML</h3>
-            <h3>CSS</h3>
-            <h3>REACT</h3>
-            <h3>SQL/NOSQL</h3>
+        <div className="aboutContainer">
+            <div className="about">
+                <h2 className="about">About Me</h2>
+                <p>Hello, I'm JJ, a software and knowledge engineering student at Kasetsart University. My main areas of
+                    interest revolve around AI, machine learning, and data processing.</p>
+                <p>"My life goal is to create a must-have thing for everyone, not just something nice to have."</p>
+            </div>
+            <div className="skill">
+                <div className="skill-item">
+                    <h3>Python </h3>
+                    <div className="line py"></div>
+                </div>
+                <div className="skill-item">
+                    <h3>Java</h3>
+                    <div className="line java"></div>
+                </div>
+                <div className="skill-item">
+                    <h3>JavaScript</h3>
+                    <div className="line js"></div>
+                </div>
+                <div className="skill-item">
+                    <h3>C++</h3>
+                    <div className="line cpp"></div>
+                </div>
+                <div className="skill-item">
+                    <h3>HTML</h3>
+                    <div className="line html"></div>
+                </div>
+                <div className="skill-item">
+                    <h3>CSS</h3>
+                    <div className="line css"></div>
+                </div>
+                <div className="skill-item">
+                    <h3>REACT</h3>
+                    <div className="line react"></div>
+                </div>
+                <div className="skill-item">
+                    <h3>SQL/NOSQL</h3>
+                    <div className="line sql"></div>
+                </div>
+            </div>
         </div>
 
     </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,11 @@ function Header(){
         <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Arapey&display=swap" rel="stylesheet" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet" />
+
     </header>;
 }
 export default Header;
